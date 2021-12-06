137c6eafa2509c325256b9c38084305d4635a47e
tanida.kazuki <tanida.kazuki@moneyforward.co.jp>
2021-12-06 10:39:41 +0900

    applynullable

--- app/graphql/mutations/create_post.rb
+++ app/graphql/mutations/create_post.rb
@@ -1,7 +1,7 @@
 module Mutations
   class CreatePost < BaseMutation
-    argument :title, String, required: true
-    argument :body, String, required: true
+    argument :title, String, required: false
+    argument :body, String, required: false 
 
     type Types::PostType
 
